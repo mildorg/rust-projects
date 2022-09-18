a2b9799856b45532558b6aed9e38c04bd71ff22a
panmin-coder <2891398008@qq.com>
2022-09-18 22:27:42 +0800

    update

--- practice/src/error_handling/option_unwrap.rs
+++ practice/src/error_handling/option_unwrap.rs
@@ -1,6 +1,7 @@
 pub fn learn() {
     // test_base_use();
-    test_question_mark();
+    // test_question_mark();
+    test_map_combinator();
 }
 
 // base use
@@ -74,5 +75,68 @@ impl Person {
 
     fn work_phone_area_code(&self) -> Option<u8> {
         self.job?.phone_number?.area_code
+    }
+}
+
+// map combinator
+fn test_map_combinator() {
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    // let potato: Option<Food> = None;
+    let potato: Option<Food> = Some(Food::Potato);
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+}
+
+#[derive(Debug)]
+enum Food {
+    Apple,
+    Carrot,
+    Potato,
+}
+
+#[derive(Debug)]
+struct Peeled(Food);
+
+#[derive(Debug)]
+struct Chopped(Food);
+
+#[derive(Debug)]
+struct Cooked(Food);
+
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None => None,
+    }
+}
+
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None => None,
+    }
+}
+
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f| Peeled(f))
+        .map(|Peeled(f)| Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None => println!("Oh no! It wasn't edible."),
     }
 }
