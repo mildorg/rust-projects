eb1c855bce409bf43c4059cfe4a56ed1cb879a77
panmin-coder <2891398008@qq.com>
2023-02-19 14:19:22 +0800

    update

--- algorithm/src/link/double_link.rs
+++ algorithm/src/link/double_link.rs
@@ -38,16 +38,14 @@ impl<T> List<T> {
         let new_head = Node::new(elem);
 
         match self.head.take() {
-            // 非空链表
             Some(old_head) => {
                 old_head.borrow_mut().pre = Some(new_head.clone());
                 new_head.borrow_mut().next = Some(old_head);
                 self.head = Some(new_head);
             }
-            // 空链表
             None => {
                 self.head = Some(new_head.clone());
-                self.tail = Some(new_head)
+                self.tail = Some(new_head);
             }
         }
     }
@@ -56,13 +54,11 @@ impl<T> List<T> {
         let new_tail = Node::new(elem);
 
         match self.tail.take() {
-            // 非空链表
             Some(old_tail) => {
                 old_tail.borrow_mut().next = Some(new_tail.clone());
                 new_tail.borrow_mut().pre = Some(old_tail);
                 self.tail = Some(new_tail);
             }
-            // 空链表
             None => {
                 self.head = Some(new_tail.clone());
                 self.tail = Some(new_tail);
@@ -73,7 +69,7 @@ impl<T> List<T> {
     pub fn pop_front(&mut self) -> Option<T> {
         self.head.take().map(|old_head| {
             match old_head.borrow_mut().next.take() {
-                // pop 后不是非空链表
+                // pop 后是非空链表
                 Some(new_head) => {
                     new_head.borrow_mut().pre.take();
                     self.head = Some(new_head);
@@ -83,6 +79,7 @@ impl<T> List<T> {
                     self.tail.take();
                 }
             }
+
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
     }
@@ -90,7 +87,7 @@ impl<T> List<T> {
     pub fn pop_back(&mut self) -> Option<T> {
         self.tail.take().map(|old_tail| {
             match old_tail.borrow_mut().pre.take() {
-                // pop 后是非空链表
+                // pop后 是非空链表
                 Some(new_tail) => {
                     new_tail.borrow_mut().next.take();
                     self.tail = Some(new_tail);
@@ -108,25 +105,25 @@ impl<T> List<T> {
     pub fn peek_front(&self) -> Option<Ref<T>> {
         self.head
             .as_ref()
-            .map(|head| Ref::map(head.borrow(), |node| &node.elem))
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
     }
 
     pub fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
         self.head
-            .as_ref()
-            .map(|head| RefMut::map(head.borrow_mut(), |node| &mut node.elem))
+            .as_mut()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
 
     pub fn peek_back(&self) -> Option<Ref<T>> {
         self.tail
             .as_ref()
-            .map(|tail| Ref::map(tail.borrow(), |node| &node.elem))
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
     }
 
     pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
         self.tail
-            .as_ref()
-            .map(|tail| RefMut::map(tail.borrow_mut(), |node| &mut node.elem))
+            .as_mut()
+            .map(|node| RefMut::map(node.borrow_mut(), |node| &mut node.elem))
     }
 }
 
