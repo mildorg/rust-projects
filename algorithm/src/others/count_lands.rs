a1a2241a5de4aae77d3f262da48ed2b25702ecf2
panmin-coder <2891398008@qq.com>
2023-03-26 10:24:43 +0800

    update count lands

--- algorithm/src/others/count_lands.rs
+++ algorithm/src/others/count_lands.rs
@@ -4,11 +4,14 @@ pub fn process() {
         vec![0, 1, 1, 1, 0, 1, 1, 1, 0],
         vec![0, 1, 1, 1, 0, 0, 0, 1, 0],
         vec![0, 1, 1, 0, 0, 1, 0, 0, 0],
+        vec![0, 0, 0, 0, 0, 1, 0, 0, 0],
+        vec![0, 0, 0, 1, 0, 0, 0, 0, 0],
     ];
 
-    let n = are.len();
-    let m = are[1].len();
+    // 遍历区域，寻找感染之后 are[i][j] = 1的数量
     let mut count = 0;
+    let n = are.len();
+    let m = are[0].len();
 
     for i in 0..n {
         (0..m).for_each(|j| {
@@ -16,7 +19,7 @@ pub fn process() {
                 count += 1;
                 infect(&mut are, i, j, n, m);
             }
-        });
+        })
     }
 
     println!("The lands count is: {count}");
@@ -27,13 +30,14 @@ pub fn process() {
 /// * j: 元素在矩阵中的列位置索引
 /// * n: 矩阵行的长度
 /// * m: 矩阵列的长度
-fn infect(are: &mut [Vec<u8>], i: usize, j: usize, n: usize, m: usize) {
+fn infect(are: &mut [Vec<i32>], i: usize, j: usize, n: usize, m: usize) {
     if i >= n || j >= m || are[i][j] != 1 {
         return;
     }
 
-    // i, j 没哟越界，且当前位置是1
+    // 将当前位置设置为2 标识为已经找到的位置
     are[i][j] = 2;
+    // 从当前位置向上下左右寻找
     infect(are, i + 1, j, n, m);
     infect(are, i, j + 1, n, m);
 
@@ -42,6 +46,6 @@ fn infect(are: &mut [Vec<u8>], i: usize, j: usize, n: usize, m: usize) {
     }
 
     if j > 0 {
-        infect(are, i, j - 1, n, m)
+        infect(are, i, j - 1, n, m);
     }
 }
