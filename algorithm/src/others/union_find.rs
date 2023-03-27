e6d99df6d61f0bfecdfb1b47a74389e2962fa62d
panmin-coder <2891398008@qq.com>
2023-03-27 21:43:00 +0800

    update union find map

--- algorithm/src/others/union_find.rs
+++ algorithm/src/others/union_find.rs
@@ -1,76 +1,58 @@
-use std::{collections::HashMap, hash::Hash, rc::Rc};
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::rc::Rc;
 
 /// 并查集
 
-/// map实现
-pub struct UnionFindMap<T> {
-    nodes_map: HashMap<Rc<T>, Rc<T>>,
-    parents_map: HashMap<Rc<T>, Rc<T>>,
-    size_map: HashMap<Rc<T>, u32>,
+/// 并查集 hash map 实现
+pub struct UnionFind<T> {
+    parents: HashMap<Rc<T>, Rc<T>>,
+    sizes: HashMap<Rc<T>, u32>,
 }
 
-impl<T: Hash + Eq> UnionFindMap<T> {
+impl<T: Hash + Eq> UnionFind<T> {
     pub fn new(list: Vec<Rc<T>>) -> Self {
-        let mut nodes_map = HashMap::new();
-        let mut parents_map = HashMap::new();
-        let mut size_map = HashMap::new();
+        let mut parents = HashMap::new();
+        let mut sizes = HashMap::new();
 
         for value in list {
-            nodes_map.insert(value.clone(), value.clone());
-            parents_map.insert(value.clone(), value.clone());
-            size_map.insert(value, 1);
+            parents.insert(value.clone(), value.clone());
+            sizes.insert(value.clone(), 1);
         }
 
-        Self {
-            nodes_map,
-            parents_map,
-            size_map,
-        }
+        Self { parents, sizes}
     }
 
     pub fn is_same_set(&mut self, a: Rc<T>, b: Rc<T>) -> bool {
-        let a = self.nodes_map.get(&a).cloned();
-        let b = self.nodes_map.get(&b).cloned();
-
-        if a.is_none() || b.is_none() {
-            return false;
-        }
-
-        self.find_parent(a.unwrap()) == self.find_parent(b.unwrap())
+        self.find_parent(a) == self.find_parent(b)
     }
 
-    pub fn union(&mut self, a: Rc<T>, b: Rc<T>) {
-        if !self.is_same_set(a.clone(), b.clone()) {
-            let a_head = self.find_parent(self.nodes_map.get(&a).unwrap().clone());
-            let b_head = self.find_parent(self.nodes_map.get(&b).unwrap().clone());
-            let a_size = self.size_map.get(&a_head).unwrap();
-            let b_size = self.size_map.get(&b_head).unwrap();
+    pub fn union(&mut self, a: Rc<T>, b:Rc<T>) {
+        let a_head = self.find_parent(a);
+        let b_head = self.find_parent(b);
 
-            let (big, small) = if a_size >= b_size {
-                (a_head, b_head)
-            } else {
-                (b_head, a_head)
-            };
+        if a_head != b_head {
+            let a_size = *self.sizes.get(&a_head).unwrap();
+            let b_size = *self.sizes.get(&b_head).unwrap();
+            let (big, small) = if a_size >= b_size {(a_head,b_head)} else {(b_head, a_head)};
 
-            self.parents_map.insert(small.clone(), big.clone());
-            self.size_map.insert(big, a_size + b_size);
-            self.size_map.remove(&small);
+            self.parents.insert(big.clone(), small.clone());
+            self.sizes.insert(big, a_size + b_size);
+            self.sizes.remove(&small);
         }
     }
 
-    // 给你一个节点，请你往上到不能再往上，把代表返回
-    fn find_parent(&mut self, el: Rc<T>) -> Rc<T> {
-        let mut cur = el;
+    pub fn find_parent(&mut self, node: Rc<T>) -> Rc<T> {
+        let mut cur = node;
         let mut path = vec![];
 
-        while cur != *self.parents_map.get(&cur).unwrap() {
+        while cur != self.parents.get(&cur).unwrap().clone() {
             path.push(cur.clone());
-            cur = self.parents_map.get(&cur).unwrap().clone();
+            cur = self.parents.get(&cur).unwrap().clone();
         }
 
         while !path.is_empty() {
-            let node = path.pop().unwrap();
-            self.parents_map.insert(node, cur.clone());
+            self.parents.insert(path.pop().unwrap(), cur.clone());
         }
 
         cur
