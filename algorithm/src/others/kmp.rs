2cc92c12286c8f88abc83145b420a3179548918a
panmin-coder <2891398008@qq.com>
2023-04-01 11:04:25 +0800

    update kmp algorithm

--- algorithm/src/others/kmp.rs
+++ algorithm/src/others/kmp.rs
@@ -1,8 +1,8 @@
 pub fn process(str1: &str, str2: &str) -> i32 {
-    let st1_len = str1.len();
+    let str1_len = str1.len();
     let str2_len = str2.len();
 
-    if str2_len == 0 || str2_len > st1_len {
+    if str2_len == 0 || str2_len > str1_len {
         return -1;
     }
 
@@ -10,11 +10,11 @@ pub fn process(str1: &str, str2: &str) -> i32 {
     let mut i1 = 0;
     let mut i2 = 0;
 
-    while i1 < st1_len && i2 < str2_len {
+    while i1 < str1_len && i2 < str2_len {
         if get_str(str1, i1) == get_str(str2, i2) {
             i1 += 1;
             i2 += 1;
-        } else if next[i2] == -1 {
+        } else if i2 == 0 {
             i1 += 1;
         } else {
             i2 = next[i2] as usize;
@@ -23,14 +23,13 @@ pub fn process(str1: &str, str2: &str) -> i32 {
 
     // 如果 i2 先越界
     match i2 == str2_len {
-        true => (i1 - i2).try_into().unwrap(),
+        true => (i1 - i2) as i32,
         false => -1,
     }
 }
 
-fn get_next_arr(s: &str) -> Vec<i32> {
-    let len = s.len();
-
+fn get_next_arr(str: &str) -> Vec<i32> {
+    let len = str.len();
     let mut next = vec![-1; len];
     next[1] = 0;
 
@@ -38,14 +37,14 @@ fn get_next_arr(s: &str) -> Vec<i32> {
     let mut cn = 0;
 
     while i < len {
-        if get_str(s, i - 1) == get_str(s, cn as usize) {
+        if get_str(str, i - 1) == get_str(str, cn as usize) {
             cn += 1;
             next[i] = cn;
             i += 1;
         } else if cn == 0 {
             i += 1;
         } else {
-            cn = next[cn as usize]
+            cn = next[cn as usize];
         }
     }
 
